7fa2aeabc1426ec53dc9bf3975804647a71058f0
vinicius945 <62552161+vinicius945@users.noreply.github.com>
2024-04-08 23:16:13 -0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,32 +1,11 @@
-print("Bem-vindo à Biblioteca FIAP")
+binario = int(input("Informe número binário: "))
+acum = 0
+pot_dois = 1
 
-opcao = 12357
+while binario != 0:
+    dig = binario % 10
+    binario = binario // 10
+    acum = acum + dig * pot_dois
+    pot_dois = pot_dois * 2
 
-while opcao != 6:
-    print("Bem-vindo à Biblioteca FIAP")
-    print("1 - Reserva Livro\n2 - Castra Livro")
-    print("3 - Devolução\n4 - Consulta Livro")
-    print("5 - Cadastro do Leitor\n6 - Sair")
-    opcao = int(input("Digite a opção desejada"))
-
-    if opcao == 1:
-        isbn = input("Informe o isbn livro: ")
-        Leitor = input("CPF dp leitor: ")
-        print("disponível a partir do dia: 25/04/2024")
-        confirmmacao = input("Deseja reservar (s/n): ")
-    elif opcao == 2:
-        pass
-    elif opcao == 3:
-        pass
-    elif opcao == 4:
-        pass
-    elif  opcao == 5:
-        nome = input("informe o nome: ") 
-        cpf = input("informe o CPF: ")
-        celular = input("Celular: ")
-        rm = int(input("RM: "))
-        print("Cadastro efetuado com sucesso!")
-    elif opcao == 6:
-        print("Até logo!")
-    else:
-        print("Opção Inválida!\n")    
+print(f"Resposta{acum}")
